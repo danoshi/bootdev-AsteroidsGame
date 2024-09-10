10b7ca914afa730cd220a16f678aacd6e948fbb3
Daniel Kaminski <danielkaminski24@gmail.com>
2024-09-10 17:52:04 +0200

    moving object

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fps = pygame.time.Clock()
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(color=BLACK)
+
+        for obj in updatable:
+            obj.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = fps.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
